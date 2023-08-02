e50da60896f92d6575afe289c594603882808f31
Wagner Lima <wagner.sousalima@gmail.com>
2023-08-02 12:01:07 -0300

    projeto integrador, Pomofit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useChallenges } from '../hooks/useChallenges';
+
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  const { level } = useChallenges();
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/WagnerSousaLima.png" alt="foto do usuário"/>
+      <div>
+        <strong>Wagner Lima</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
